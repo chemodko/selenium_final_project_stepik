f8174120aa8e824c71974eb6d6450ae287a5fd35
Artem Chemodanov <artem_chemodanov2000@mail.ru>
2023-11-23 16:29:08 +0300

    update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
-
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
 
 
 
