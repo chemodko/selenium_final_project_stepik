8137bb40c47672d86c299d2f435d69a5184a8639
Artem Chemodanov <artem_chemodanov2000@mail.ru>
2023-11-23 17:06:24 +0300

    update page changes in test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -10,6 +10,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page2 = LoginPage(browser, browser.current_url)   # переходим на странницу Login
+    page2.should_be_login_page()                      # проверяем корректность страницы login
 
 
 def test_guest_should_see_login_link(browser):
@@ -18,9 +20,9 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
-def test_guest_should_see_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    page2 = LoginPage(browser, browser.current_url)
-    page2.should_be_login_page()
+# def test_guest_should_see_login_page(browser):
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+#     page2 = LoginPage(browser, browser.current_url)
+#     page2.should_be_login_page()
