394689fa64b56d06d4411f2800200c90ad3b226c
Artem Chemodanov <artem_chemodanov2000@mail.ru>
2023-11-23 17:02:45 +0300

    add login_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -5,3 +5,6 @@ class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
+class LoginPageLocators:
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -16,3 +17,10 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+
+def test_guest_should_see_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page2 = LoginPage(browser, browser.current_url)
+    page2.should_be_login_page()
