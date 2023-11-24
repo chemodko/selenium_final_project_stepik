73932eb5067e0a11b870f781b5686a182b0bda0b
Artem Chemodanov <artem_chemodanov2000@mail.ru>
2023-11-24 12:13:06 +0300

    add xfailed link

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,20 @@
 from .pages.product_page import ProductPage
 import time
+import pytest
 
 
-link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+# link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+# link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+
+xfail_num = 7
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+links = [product_base_link + str(num) for num in range(10) if num != xfail_num]
+xlink = pytest.param(product_base_link + str(xfail_num), marks=pytest.mark.xfail(reason="mistake on page"))
+links.insert(xfail_num, xlink)
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_cart_button()
